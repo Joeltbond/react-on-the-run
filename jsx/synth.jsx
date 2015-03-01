e8d358fbfd8c077a198b5c1defc09f001daba064
Joel Bond <joeltbond@gmail.com>
2015-03-01 09:29:38 -0800

    set up event handlers for sequencer

--- jsx/synth.jsx
+++ jsx/synth.jsx
@@ -22,15 +22,36 @@
     var Sequencer = React.createClass({
         render() {
             var columns = [];
-            for (var i = 0; i < 8; i++) {
-                columns.push(<StepColumn selectedNote={i} />);
-            }
+
+            this.state.pattern.forEach((note, index) => {
+                columns.push(<StepColumn
+                    note={note}
+                    step={index}
+                    key={index}
+                    onNoteChange={this.handleNoteChange}
+                    />);
+            });
+
             return (
                 <div>
                     Sequencer
                     {columns}
                 </div>
             );
+        },
+
+        getInitialState() {
+            return {
+                pattern: [0, 1, 2, 3, 4, 5, 6, 7]
+            };
+        },
+
+        handleNoteChange(step, note) {
+            var pattern = this.state.pattern.slice();
+            pattern[step] = note;
+            this.setState({
+                pattern: pattern
+            });
         }
     });
     var StepColumn = React.createClass({
@@ -39,13 +60,20 @@
             for (var i = 0; i < 8; i++) {
                 buttons.push(<input type="radio"
                     data-note={i}
-                    checked={i === this.props.selectedNote} />);
+                    checked={i === this.props.note}
+                    key={i}
+                    onClick={this.handleClick.bind(this, i)}
+                    />);
             }
             return (
                 <form>
                     {buttons}
                 </form>
             );
+        },
+
+        handleClick(note) {
+            this.props.onNoteChange(this.props.step, note);
         }
     });
 
