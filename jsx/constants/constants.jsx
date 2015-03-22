e4cb343e84efd49920a1a3e8191a45b7979599d2
Joel Bond <joeltbond@gmail.com>
2015-03-22 16:40:45 +0000

    Some initial setup

---
+++ jsx/constants/constants.jsx
@@ -0,0 +1,10 @@
+insert keyMirror from ('keyMirror');
+
+export keyMirror({
+    SYNTH_NOTE_CHANGE: null,
+    SYNTH_STEP: null,
+    SYNTH_START: null,
+    SYTHN_STOP: null,
+    SYNTH_PLAY_SOUND: null,
+    SYNTH_MUTE_SOUND: null
+});
